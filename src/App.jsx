38f750e2848e05e0af3c4940b57791432f94fda6
Olafey <olamidearemu2000@yahoo.com>
2025-03-04 10:52:46 +0100

    more components

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import LandingPage from "./components/LandingPage";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <>
       <div>
+        <NavBar />
         <LandingPage />
       </div>
     </>
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <div>
+      <h1>Logo</h1>
+      <div>
+        <a href="#">Home</a>
+        <a href="#">About</a>
+        <a href="#">Contact</a>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
