9b90504d645cd9ec8bd4bb427ca942791a49ed82
Olafey <olamidearemu2000@yahoo.com>
2025-03-05 10:26:29 +0100

    ahref

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import "./App.css";
 import LandingPage from "./pages/LandingPage";
 import NavBar from "./components/NavBar";
 import Button from "./components/Button";
-import profileCard from "./components/profileCard";
+import ProfileCard from "./components/profileCard";
 
 function App() {
   const signup = () => {
@@ -13,7 +13,12 @@ function App() {
       <div>
         <NavBar />
         <LandingPage />
-        <profileCard />
+        <ProfileCard
+          image="https://images.pexels.com/photos/16171084/pexels-photo-16171084/free-photo-of-otter-lying-on-rocks.jpeg?auto=compress&cs=tinysrgb&w=1200&lazy=load"
+          name="Olamide"
+          email="olafey@gmail.com"
+          age="90"
+        />
       </div>
     </>
   );
--- src/components/profileCard.jsx
+++ src/components/profileCard.jsx
@@ -1,13 +1,21 @@
 import React from "react";
+import Button from "./Button";
 
-const profileCard = (props) => {
+const ProfileCard = (props) => {
   return (
-    <div>
-      <p>Name: James bond</p>
-      <p>Email: James@gmail.com</p>
-      <p>Age: 9</p>
+    <div className="card">
+      <img src={props.image} alt="" width={100} />
+      <p>Name: {props.name}</p>
+      <p>Email: {props.email}</p>
+      <p>Age: {props.age}</p>
+      <a
+        href={props.link}
+        style={{ background: "red", color: "white", padding: ".5rem 1rem" }}
+      >
+        Website
+      </a>
     </div>
   );
 };
 
-export default profileCard;
+export default ProfileCard;
