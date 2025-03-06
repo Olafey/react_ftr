9445f244b8ab8ed7b50fa0f5c39eaaccdfad4d5e
Olafey <olamidearemu2000@yahoo.com>
2025-03-06 10:29:24 +0100

    useEffect

---
+++ src/components/Products.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { useState } from "react";
+
+const Products = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [search, setSearch] = useState("");
+  const baseUrl = "https://fakestoreapi.com";
+  return <div></div>;
+};
+
+export default Products;
