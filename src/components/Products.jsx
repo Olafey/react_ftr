d92ca5b433a9a29c27d02ca69038c910fe738b7c
Olafey <olamidearemu2000@yahoo.com>
2025-03-06 10:34:27 +0100

    m

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -5,7 +5,20 @@ const Products = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [search, setSearch] = useState("");
   const baseUrl = "https://fakestoreapi.com";
-  return <div></div>;
+
+  //useEffect
+  return (
+    <div>
+      {isLoading ? (
+        <p>Loading....</p>
+      ) : (
+        <div>
+          <h1>Product title</h1>
+        </div>
+      )}
+      <button>Get Products</button>
+    </div>
+  );
 };
 
 export default Products;
