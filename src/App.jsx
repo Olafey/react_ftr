23545dc64d24802470a1ca52bef2b3e07d9a62ea
Olafey <olamidearemu2000@yahoo.com>
2025-03-12 09:42:19 +0100

    routing

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import Button from "./components/Button";
 import ProfileCard from "./components/profileCard";
 import Products from "./components/products";
 
+import NotFound from "./pages/NotFound";
+
 import Counter from "./components/Counter";
 
 // function App() {
@@ -36,6 +38,7 @@ import Counter from "./components/Counter";
 // export default App;
 
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
@@ -44,8 +47,8 @@ const App = () => {
         <Routes>
           <Route path="/" element={<LandingPage />} />
           <Route path="/products" element={<Products />} />
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/" element={<LandingPage />} />
+          <Route path="/profilecards" element={<ProfileCard />} />
+          <Route path="/counter/show" element={<Counter />} />
         </Routes>
       </BrowserRouter>
     </>
