1f3296292e3155e503c392ee1cbce36282198f20
Olafey <olamidearemu2000@yahoo.com>
2025-03-12 09:26:25 +0100

    routing

--- src/App.jsx
+++ src/App.jsx
@@ -7,30 +7,49 @@ import Products from "./components/products";
 
 import Counter from "./components/Counter";
 
-function App() {
-  const signup = () => {
-    alert("signup");
-  };
+// function App() {
+//   const signup = () => {
+//     alert("signup");
+//   };
+//   return (
+//     <>
+//       <div>
+//         {
+//           // <NavBar />
+//           /*   <LandingPage />
+//         <ProfileCard
+//           image="https://images.pexels.com/photos/16171084/pexels-photo-16171084/free-photo-of-otter-lying-on-rocks.jpeg?auto=compress&cs=tinysrgb&w=1200&lazy=load"
+//           name="Olamide"
+//           email="olafey@gmail.com"
+//           age="90"
+//         /> */
+//         }
+//         {/*
+//         <Counter /> */}
+
+//         {/* <Products /> */}
+//       </div>
+//     </>
+//   );
+// }
+
+// export default App;
+
+import React from "react";
+
+const App = () => {
   return (
     <>
-      <div>
-        {
-          <NavBar />
-          /*   <LandingPage />
-        <ProfileCard
-          image="https://images.pexels.com/photos/16171084/pexels-photo-16171084/free-photo-of-otter-lying-on-rocks.jpeg?auto=compress&cs=tinysrgb&w=1200&lazy=load"
-          name="Olamide"
-          email="olafey@gmail.com"
-          age="90"
-        /> */
-        }
-        {/* 
-        <Counter /> */}
-
-        <Products />
-      </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/" element={<LandingPage />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
-}
+};
 
 export default App;
